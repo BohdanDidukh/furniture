bb5574f69286ecade8211300c2ac7906574a90df
BohdanDidukh <bodjandi@gmail.com>
2023-04-05 12:43:15 +0300

    Project settings

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+
+import styles from "./App.module.scss";
+
+interface AppProps {}
+
+const App: FC<AppProps> = () => (
+  <div className={styles.App}>
+  </div>
+);
+
+export default App;
