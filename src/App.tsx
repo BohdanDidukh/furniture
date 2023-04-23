85386ddb17140cbb92094ae7b5c76f4fd8507315
BohdanDidukh <bodjandi@gmail.com>
2023-04-23 20:26:40 +0300

    Add Testimonials

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,16 @@ import Hero from "./components/Hero/Hero";
 import Shop from "./components/Shop/Shop";
 
 import styles from "./App.module.scss";
+import Reviews from "./components/Reviews/Reviews";
 
 interface AppProps {}
 
 const App: FC<AppProps> = () => (
   <div className={styles.App}>
-    <Hero/>
-    <Features/>
+    <Hero />
+    <Features />
     <Shop />
+    <Reviews />
   </div>
 );
 
