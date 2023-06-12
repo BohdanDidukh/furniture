c44e181151c46390a3735669f68a7b0f19dc1334
BohdanDidukh <bodjandi@gmail.com>
2023-06-13 00:17:01 +0300

    Add product search

--- src/store/slices/productSlice.ts
+++ src/store/slices/productSlice.ts
@@ -1,20 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 import products from "../../data/data";
+import { Product } from "../../interfaces/Product";
+
+interface ProductState {
+  filters: { category: string; position: string }[];
+  activeFilter: string;
+  products: Product[];
+  filteredProducts: Product[];
+  searchTerm: string;
+  searchResult: Product[];
+}
+
+const initialState: ProductState = {
+  filters: [
+    { category: "Chairs", position: "0 " },
+    { category: "Lamps", position: "25%" },
+    { category: "Beds", position: "50%" },
+    { category: "Sofa", position: "75%" },
+  ],
+  activeFilter: "Chairs",
+  products: products,
+  filteredProducts: products,
+  searchTerm: "",
+  searchResult: [],
+};
 
 const productSlice = createSlice({
   name: "product",
-  initialState: {
-    filters: [
-      { category: "Chairs", position: "0 " },
-      { category: "Lamps", position: "25%" },
-      { category: "Beds", position: "50%" },
-      { category: "Sofa", position: "75%" },
-    ],
-    activeFilter: "Chairs",
-    products: products,
-    filteredProducts: products,
-  },
+  initialState,
   reducers: {
     setActiveFilter: (state, action) => {
       state.activeFilter = action.payload;
@@ -22,8 +36,14 @@ const productSlice = createSlice({
         (product) => product.category.name === action.payload
       );
     },
+    setSearchTerm: (state, action) => {
+      state.searchTerm = action.payload;
+      state.searchResult = state.products.filter((product) =>
+        product.name.toLowerCase().includes(action.payload.toLowerCase())
+      );
+    },
   },
 });
 
-export const { setActiveFilter } = productSlice.actions;
+export const { setActiveFilter, setSearchTerm } = productSlice.actions;
 export default productSlice.reducer;
